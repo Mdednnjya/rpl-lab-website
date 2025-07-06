6406abbd71f70cb0c834557c31f441f141186c8a
Danan <narayanmade578@gmail.com>
2025-07-06 17:19:02 +0700

    chore: setup project structure

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximmum-scale=1.0">
+    @ViteReactRefresh
+    @vite('resource/jss/app.jsx')
+    @inertiaHead
+    <title>RPL Lab Website</title>
+</head>
+<body>
+    @inertia
+</body>
+</html>
